2c112a855cd9405df769f098f4acc91143abe4f9
arielLevita <levita.ariel@gmail.com>
2024-07-12 21:01:43 -0300

    AudioSelector added.

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,25 @@
 import './App.css'
 import Navbar from './components/Navbar';
 import AudioSelector from './pages/AudioSelector';
-import TaskConfiguration from './pages/TaskConfiguration';
+// import TaskConfiguration from './pages/TaskConfiguration';
 
 function App() {
   return (
     <>
-      <div className='max-w-md mx-auto border border-black'>
+      <div className='flex flex-col h-screen max-w-md mx-auto border border-black'>
 
         <div>navbar</div>
 
-        <div className='p-4 border border-green-700'>
-          <div className='border border-red-700'>
-            <TaskConfiguration />
+        <div className='flex-grow-1 h-full p-4 border border-green-700'>
+          <div>
+            {/* <TaskConfiguration /> */}
             <AudioSelector />
           </div>
         </div>
 
-        <Navbar />
+        <div>
+          <Navbar />
+        </div>
       </div>
     </>
   )
--- src/pages/AudioSelector.jsx
+++ src/pages/AudioSelector.jsx
@@ -8,17 +8,49 @@ const AudioSelector = () => {
 
     const audios = [
         {
+            id: 'musicLoFi',
             name: 'musicLoFi',
             value: 'musicLoFi',
             label: 'Música Lo-Fi',
-            source: 'https://cdn.pixabay.com/audio/2024/01/15/audio_9914e58808.mp3'
+            // source: 'https://cdn.pixabay.com/audio/2024/01/15/audio_9914e58808.mp3'
+            source: ''
         },
         {
+            id: 'pianoMelody',
+            name: 'pianoMelody',
+            value: 'pianoMelody',
+            label: 'Melodía de piano',
+            source: ''
+        },
+        {
+            id: 'guitarMelody',
+            name: 'guitarMelody',
+            value: 'guitarMelody',
+            label: 'Melodía de guitarra',
+            source: ''
+        },
+        {
+            id: 'rainSound',
             name: 'rainSound',
             value: 'rainSound',
             label: 'Sonido de lluvia',
-            source: 'https://cdn.pixabay.com/download/audio/2024/05/21/audio_08ef8717b4.mp3'
-        }
+            // source: 'https://cdn.pixabay.com/download/audio/2024/05/21/audio_08ef8717b4.mp3'
+            source: ''
+        },
+        {
+            id: 'waveSound',
+            name: 'waveSound',
+            value: 'waveSound',
+            label: 'Sonido de olas',
+            source: ''
+        },
+        {
+            id: 'noSound',
+            name: 'noSound',
+            value: 'noSound',
+            label: 'Sin música',
+            source: ''
+        },
     ]
 
     useEffect(() => {
@@ -46,18 +78,49 @@ const AudioSelector = () => {
 
     return (
         <>
-            <div>
+            <div className="grid gap-4">
                 {
-                    audios?.map((audio) => (
+                    audios?.map((audio, index) => (
                         <div key={audio.name}>
-                            <input
-                                type="radio"
-                                name="audio"
-                                id={audio.id}
-                                value={audio.value}
-                                onChange={handleAudioChange}
-                            />
-                            <label htmlFor={audio.id}>{audio.label}</label>
+                            <label
+                                className="w-full flex justify-between bg-celeste has-[:checked]:bg-azul text-black has-[:checked]:text-white fill-black has-[:checked]:fill-white py-2 px-4"
+                                htmlFor={audio.id}
+                            >
+                                <span className="flex">
+                                    {index !== audios.length - 1 && (
+                                        <svg className="h-6 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-422h240v160H560v400q0 66-47 113t-113 47Z" /></svg>
+                                    )}
+                                    {index === audios.length - 1 && (
+                                        <svg className="h-6 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M792-56 56-792l56-56 736 736-56 56ZM560-514l-80-80v-246h240v160H560v166ZM400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-62l80 80v120q0 66-47 113t-113 47Z"/></svg>
+                                    )}
+                                    {audio.label}
+                                </span>
+                                <input
+                                    className="
+                                            w-4
+                                            aspect-square
+                                            content-center 
+                                            before:block 
+                                            before:[content:''] 
+                                            before:w-full 
+                                            before:aspect-square 
+                                            before:rounded-full 
+                                            before:bg-celeste
+                                            before:border-2
+                                            before:border-naranja
+                                            checked:before:bg-naranja 
+                                            checked:before:ring-naranja 
+                                            checked:before:ring-2 
+                                            checked:before:border-2 
+                                            checked:before:border-azul"
+                                    type="radio"
+                                    name="audio"
+                                    id={audio.id}
+                                    value={audio.value}
+                                    onChange={handleAudioChange}
+                                    defaultChecked={index === 0}
+                                />
+                            </label>
                         </div>
                     ))
                 }
