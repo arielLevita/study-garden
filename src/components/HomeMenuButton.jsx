8734067c055aa932ccefb907760868e3d78a29b1
arielLevita <levita.ariel@gmail.com>
2024-08-15 19:59:07 -0300

    Topbar added.

---
+++ src/components/HomeMenuButton.jsx
@@ -0,0 +1,9 @@
+const HomeMenuButton = () => {
+    return (
+        <div className="p-2">
+            <svg className="w-8 aspect-square fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
+        </div>
+    )
+}
+
+export default HomeMenuButton
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,12 +1,46 @@
+import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom"
+import HomeMenuButton from "./HomeMenuButton"
 import GoBackButton from "./GoBackButton"
 
-
 const Topbar = () => {
+
+    const [currentPath, setCurrentPath] = useState('')
+    const location = useLocation();
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case '/newtask/taskconfiguration':
+                setCurrentPath('Diseño');
+                break;
+            case '/newtask/taskdescription':
+                setCurrentPath('Descripción');
+                break;
+            case '/newtask/audioselector':
+                setCurrentPath('Audio');
+                break;
+            case '/underconstruction':
+                setCurrentPath('En construcción');
+                break;
+            case '/':
+            default:
+                setCurrentPath('Inicio');
+                break;
+        }
+    }, [location])
+
     return (
         <div className='flex items-center h-20 w-full bg-azul'>
             <div className="max-w-lg w-full mx-auto">
-                <div className="flex justify-start p-2">
-                    <GoBackButton />
+                <div className="flex justify-start items-center p-2">
+                    {
+                        location.pathname == '/' 
+                        ? <HomeMenuButton />
+                        : <GoBackButton /> 
+                    }
+                    <div>
+                        <button type='button' className="text-white text-2xl mx-4">{currentPath}</button>
+                    </div>
                 </div>
             </div>
         </div>
